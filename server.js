3dcb73a88335f5927ea4d51533462d95e82c2093
Jessica <sevorg.jess@gmail.com>
2022-01-02 21:13:29 -0700

    Add heroku

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+
+// require the data from the JSON
+const { animals } = require('./data/animals');
+
+// instantiate the server
+const app = express();
+
+// handle filtering in a separate function
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  //note that we save the animalsArray as filteredResults here:
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    // save personality traits as a dedicated array.
+    // if personalityTraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // Loop through each trait in the personalityTraits array
+    personalityTraitsArray.forEach(trait => {
+      // check the trait against each animal in the filteredREsults array
+      // Remember, it is initially a copy of the animalsArray,
+      // but here we're updating it for each trait in the .forEach() loop
+      // For each trait being targeted by the filter, the filteredResults
+      // array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one
+      // of the traits when the .forEach() loop is finished.
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
+
+
+// add the route - get requires two arguments 
+// 1. a string describing the route the client will fetch from
+// 2. a callback function that will execute every time that the route is accessed with a GET request
+// the .send method on the res parameter sends the string to the client
+app.get('/api/animals', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+// tell the server to listen for requests
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
